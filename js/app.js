f045eb579af2f6e815ac8642ee67097c7843fc8d
pgarbrecht <philgarbrecht@gmail.com>
2022-07-13 21:39:37 -0500

    made basic player class

--- js/app.js
+++ js/app.js
@@ -4,3 +4,21 @@ console.log("dbz rules");
 window.onload = function welcome() {
     var popup = document.getElementById("welcomePopup");  popup.classList.toggle("show");
   };
+
+//Event listeners for character selection
+const gokuChoice = document.querySelector("#goku");
+const vegetaChoice = document.querySelector("#vegeta");
+const gohanChoice = document.querySelector("#gohan");
+const trunksChoice = document.querySelector("#trunks");
+
+//Class for characters
+class Player {
+    //static property to remember # of players created
+    static lastPlayer = 0;
+
+    constructor(player,hp,ki) {
+        this.player = ++Player.lastPlayer;
+        this.hp = hp || 100
+        this.ki = ki || 0
+    }
+}
