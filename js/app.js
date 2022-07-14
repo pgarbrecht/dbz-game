a33b60394c85cceb0b71ae84fc78ab544fa82227
pgarbrecht <philgarbrecht@gmail.com>
2022-07-13 19:43:44 -0500

    added basic welcome pop up

--- js/app.js
+++ js/app.js
@@ -1,1 +1,6 @@
 console.log("dbz rules");
+
+// When the page loads, open the welcome popup
+window.onload = function welcome() {
+    var popup = document.getElementById("welcomePopup");  popup.classList.toggle("show");
+  };
