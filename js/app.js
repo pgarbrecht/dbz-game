dd5e237b4167ccd985e5b1cc6893f0513f8408f1
pgarbrecht <philgarbrecht@gmail.com>
2022-07-15 21:10:56 -0500

    added prompt and ki disappear functionality

--- js/app.js
+++ js/app.js
@@ -60,12 +60,20 @@ function removeWelcome() {
 }
 
 //elements needed once game starts
+const p1Info = document.querySelector(".p1-info");
 let p1Image = document.querySelector(".p1-image");
 const p1Name = document.querySelector(".p1-name");
 let p1HP = document.querySelector(".p1-hp");
+let p1Ki = document.querySelector(".p1-ki");
+
+const prompt = document.querySelector(".prompt");
+const promptText = document.querySelector(".prompt-text");
+
+const p2Info = document.querySelector(".p2-info");
 let p2Image = document.querySelector(".p2-image");
 const p2Name = document.querySelector(".p2-name");
 let p2HP = document.querySelector(".p2-hp");
+let p2Ki = document.querySelector(".p2-ki");
 
 function startGame() {
     // assign objects to player variables
@@ -73,11 +81,17 @@ function startGame() {
     p2 = players[1];
     // show their images, health character names, prompt 1
     p1Image.setAttribute("src",p1.images[0]);
-    p1Name.textContent = `P1:${p1.character}`;
-    p1HP.textContent = `${p1.hp}HP`;
+    p1Name.textContent = `P1: ${p1.character}`;
+    p1HP.textContent = `${p1.hp} HP`;
+    p1Ki.textContent = `${p1.ki} Ki`;
     p2Image.setAttribute("src",p2.images[0]);
-    p2Name.textContent = `P2:${p2.character}`;
-    p2HP.textContent = `${p2.hp}HP`;
+    p2Name.textContent = `P2: ${p2.character}`;
+    p2HP.textContent = `${p2.hp} HP`;
+    p2Ki.textContent = `${p2.ki} Ki`;
+    p1Info.style.backgroundColor = "orange";
+    p2Info.style.backgroundColor = "orange";
+    prompt.style.backgroundColor = "white";
+    promptText.textContent = `P1 has 15 seconds to collect ki!`;
     kiCollect();
 }
 
@@ -85,7 +99,7 @@ const gridItems = document.querySelectorAll(".grid-item");
 const ki = document.querySelector(".ki");
 let result = 0;
 let hitPosition;
-let currentTime = 30;
+let currentTime = 15;
 let timerId = null;
 
 function kiCollect() {
@@ -110,16 +124,21 @@ function kiCollect() {
     })
 
     function moveKi() {
-        timerId = setInterval(randomGridItem, 750)
+        timerId = setInterval(randomGridItem, 500)
     }
 
     moveKi();
 
     function countDown() {
         currentTime--;
+        promptText.textContent = `P1 has ${currentTime} seconds to collect ki!`;
         if(currentTime == 0) {
             clearInterval(countDownTimerId);
             clearInterval(timerId);
+            //need code to remove ki picture
+            gridItems.forEach(item => {
+                item.classList.remove('ki')
+            })
         }
     }
 
